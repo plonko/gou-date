2b2d038304a33e7f6d0286af80c0369c214553af
Seb <sebastian@livingmap.com>
2015-03-20 16:09:30 +0000

    Added some barmey Jade stuff to print he dates, added clicks for date delivery state toggling

--- build/media/js/script.js
+++ build/media/js/script.js
@@ -1,3 +1,11 @@
-var msg = 'hi';
-console.log(msg);
-//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbInNjcmlwdC5qcyJdLCJuYW1lcyI6W10sIm1hcHBpbmdzIjoiQUFBQTtBQUNBIiwiZmlsZSI6InNjcmlwdC5qcyIsInNvdXJjZXNDb250ZW50IjpbInZhciBtc2cgPSAnaGknO1xuY29uc29sZS5sb2cobXNnKTsiXSwic291cmNlUm9vdCI6Ii9zb3VyY2UvIn0=
+var classNames = ['delivery-day set','delivery-day hold'];
+
+$('.delivery-day').click(function(){
+    $(this).siblings().removeClass('set hold');
+    $(this).toggleClass(function (i, className) {
+        var index = (classNames.indexOf(className) + 1) % classNames.length;
+        $(this).removeClass(className);
+        return classNames[index];
+    });
+});
+//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbInNjcmlwdC5qcyJdLCJuYW1lcyI6W10sIm1hcHBpbmdzIjoiQUFBQTtBQUNBO0FBQ0E7QUFDQTtBQUNBO0FBQ0E7QUFDQTtBQUNBO0FBQ0E7QUFDQSIsImZpbGUiOiJzY3JpcHQuanMiLCJzb3VyY2VzQ29udGVudCI6WyJ2YXIgY2xhc3NOYW1lcyA9IFsnZGVsaXZlcnktZGF5IHNldCcsJ2RlbGl2ZXJ5LWRheSBob2xkJ107XG5cbiQoJy5kZWxpdmVyeS1kYXknKS5jbGljayhmdW5jdGlvbigpe1xuICAgICQodGhpcykuc2libGluZ3MoKS5yZW1vdmVDbGFzcygnc2V0IGhvbGQnKTtcbiAgICAkKHRoaXMpLnRvZ2dsZUNsYXNzKGZ1bmN0aW9uIChpLCBjbGFzc05hbWUpIHtcbiAgICAgICAgdmFyIGluZGV4ID0gKGNsYXNzTmFtZXMuaW5kZXhPZihjbGFzc05hbWUpICsgMSkgJSBjbGFzc05hbWVzLmxlbmd0aDtcbiAgICAgICAgJCh0aGlzKS5yZW1vdmVDbGFzcyhjbGFzc05hbWUpO1xuICAgICAgICByZXR1cm4gY2xhc3NOYW1lc1tpbmRleF07XG4gICAgfSk7XG59KTsiXSwic291cmNlUm9vdCI6Ii9zb3VyY2UvIn0=
--- src/media/js/scripts/script.js
+++ src/media/js/scripts/script.js
@@ -1,2 +1,10 @@
-var msg = 'hi';
-console.log(msg);
+var classNames = ['delivery-day set','delivery-day hold'];
+
+$('.delivery-day').click(function(){
+    $(this).siblings().removeClass('set hold');
+    $(this).toggleClass(function (i, className) {
+        var index = (classNames.indexOf(className) + 1) % classNames.length;
+        $(this).removeClass(className);
+        return classNames[index];
+    });
+});
