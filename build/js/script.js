82288e190cbdf0019bffdaf49e2c126c85907a0c
Seb <sebastian@livingmap.com>
2015-03-20 12:04:12 +0000

    Added gulp tasks for JS +  uglify, concat and sourcemaps

---
+++ build/js/script.js
@@ -0,0 +1,3 @@
+var msg = 'hi';
+console.log(msg);
+//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbInNjcmlwdC5qcyJdLCJuYW1lcyI6W10sIm1hcHBpbmdzIjoiQUFBQTtBQUNBIiwiZmlsZSI6InNjcmlwdC5qcyIsInNvdXJjZXNDb250ZW50IjpbInZhciBtc2cgPSAnaGknO1xuY29uc29sZS5sb2cobXNnKTsiXSwic291cmNlUm9vdCI6Ii9zb3VyY2UvIn0=
--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,48 @@
 var gulp = require('gulp');
 var less = require('gulp-less');
 var jade = require('gulp-jade');
- 
+
+var uglify = require('gulp-uglify');
+var concat = require('gulp-concat');
+var gutil = require('gulp-util');
+var source = require('gulp-sourcemaps');
+
+var jsLibs = ['src/media/js/libs/zepto.js'];
+var jsScripts = ['src/media/js/scripts/script.js'];
+
+// Run 'gulp --dev' for unminned and sourcemaps
+
+var isProduction = true;
+var sourceMap = false;
+
+if(gutil.env.dev === true) {
+    sourceMap = true;
+    isProduction = false;
+}
+
+gulp.task('js-libs', function() {
+    return gulp.src(jsLibs)
+        .pipe(isProduction ? uglify().on('error', errorHandler) : gutil.noop())
+        .pipe(concat('libs.js'))
+        .pipe(gulp.dest('./build/js/'));
+});
+
+gulp.task('js-scripts', function() {
+    return gulp.src(jsScripts)
+        .pipe(sourceMap ? source.init() : gutil.noop())
+            .pipe(isProduction ? uglify().on('error', errorHandler) : gutil.noop())
+            .pipe(concat('script.js'))
+        .pipe(sourceMap ? source.write() : gutil.noop())
+        .pipe(gulp.dest('./build/js/'));
+});
+
 gulp.task('less', function () {
     return gulp.src('src/media/less/master.less')
-        .pipe(less())
+        .pipe(sourceMap ? source.init() : gutil.noop())
+            .pipe(less({
+                compress: isProduction
+            }))
+        .pipe(sourceMap ? source.write() : gutil.noop())
         .pipe(gulp.dest('./build/media/css/'));
 });
  
@@ -16,8 +54,16 @@ gulp.task('jade', function () {
 
 // Rerun the task when a file changes
 gulp.task('watch', function() {
+    gulp.watch(jsLibs, ['js-libs']);
+    gulp.watch(jsScripts, ['js-scripts']);
     gulp.watch('src/media/less/**/*.less', ['less']);
     gulp.watch('src/templates/**/*.jade', ['jade']);
 });
 
-gulp.task('default', ['watch', 'less', 'jade']);
+gulp.task('default', ['watch', 'js-libs', 'js-scripts', 'less', 'jade']);
+
+// Handle the error
+function errorHandler (error) {
+    console.log(error.toString());
+    this.emit('end');
+}
